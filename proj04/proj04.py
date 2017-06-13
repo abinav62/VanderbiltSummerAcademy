1fd395155631be3358439172974464e2046f9442
Peabody <peabody@dhcp-129-59-214-98.n1.vanderbilt.edu>
2017-06-13 10:43:25 -0500

    proj 4

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -8,3 +8,27 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+string = raw_input("Type in a word: ")
+derp = list(string)
+stringList = []
+for x in derp:
+    if x != ' ':
+        stringList.append(x)
+    else:
+        pass
+li = []
+
+for letter in stringList[-1:0:-1]:
+    if letter != ' ':
+        li.append(letter)
+    else:
+        pass
+
+li.append(stringList[0])
+str1 = ''.join(li)
+str2 = ''.join(stringList)
+
+if str1.lower() == str2.lower():
+    print "This word or phrase is a palindrome."
+else:
+    print "This word or phrase is not a palindrome"
